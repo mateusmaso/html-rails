301dab5f676ea0af16470287140609401c4fe1d7
mateusmaso <m.maso25@gmail.com>
2013-07-29 01:28:11 -0300

    whitespace fix

--- lib/html/rails/engine.rb
+++ lib/html/rails/engine.rb
@@ -1,10 +1,10 @@
 module Html
   module Rails
-	  class Engine < ::Rails::Engine
-	  	config.before_initialize do |app|
-	  		Sprockets::Engines
-	  		Sprockets.register_engine('.html', Tilt)
-	  	end
-	  end
-	end
+    class Engine < ::Rails::Engine
+      config.before_initialize do |app|
+        Sprockets::Engines
+        Sprockets.register_engine('.html', Tilt)
+      end
+    end
+  end
 end
